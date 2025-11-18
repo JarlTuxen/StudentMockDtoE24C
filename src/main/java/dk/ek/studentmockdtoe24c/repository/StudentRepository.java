e39312fcd151369484e4293eb2624616ec28fac1
Jarl Tuxen <jart@kea.dk>
2025-11-18 09:32:39 +0100

    start på mockito og mockmvc

--- src/main/java/dk/ek/studentmockdtoe24c/repository/StudentRepository.java
+++ src/main/java/dk/ek/studentmockdtoe24c/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package dk.ek.studentmockdtoe24c.repository;
 
-public interface StudentRepository {
+import dk.ek.studentmockdtoe24c.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
